3fdbda6c062c73c946a81378af6f9230f3eb37e8
adamj3142 <adamj3142@gmail.com>
2026-08-15 20:57:21 +0100

    Create player healthbar

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,6 +4,19 @@ extends CharacterBody2D
 @export var move_speed: float = 250.0
 var projectile_scene: PackedScene = preload("res://scenes/projectiles/playerbullet/playerbullet.tscn")
 
+@onready var health_component: HealthComponent = $Components/HealthComponent
+@onready var health_bar: ProgressBar = $HUD/HealthBar
+
+func _ready() -> void:
+	health_bar.max_value = health_component.max_health
+	health_bar.value = health_component.current_health
+
+	health_component.health_changed.connect(_on_health_changed)
+
+func _on_health_changed(current_health: float, max_health: float) -> void:
+	health_bar.max_value = max_health
+	health_bar.value = current_health
+	
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector(
 		"move_left",
